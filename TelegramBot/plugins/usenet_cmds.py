c817136348f732370e3f9f9a71c2e8c8d90d47c0
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2023-03-05 12:09:05 +0530

    Update usenet_cmds.py

--- TelegramBot/plugins/usenet_cmds.py
+++ TelegramBot/plugins/usenet_cmds.py
@@ -1,257 +1,173 @@
 import re
-import time
 import httpx
-import aiofiles
-import asyncio 
-import requests
-import urllib.parse
+import asyncio
+import requests 
 
-from shutil import disk_usage 
-from datetime import datetime 
-from time import time as timefunc
-from psutil import cpu_percent, virtual_memory
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-from TelegramBot.config import SUDO_USERID 
-from TelegramBot import bot, scheduler, loop
-from TelegramBot import BotStartTime, SABNZBD_ENDPOINT 
-from TelegramBot.helpers.functions import get_readable_bytes
-from TelegramBot.helpers.pasting_services import katbin_paste 
-from TelegramBot.helpers.functions import get_readable_time 
+from TelegramBot.config import SUDO_USERID
+from TelegramBot import NZBHYDRA_URL_ENDPOINT  
+from TelegramBot.usenetbot.sabnzbd import UsenetBot 
+from TelegramBot.helpers.filters import check_auth, sudo_cmd
+
+sabnzbd_userid_log: dict = {} #saves userid of user along with his task id. {userid:[task1, task2]}
+    
+    
+usenetbot = UsenetBot () 
+@Client.on_message(filters.command("pstatus") & check_auth)
+async def downloading_status(client: Client, message: Message):
+    await usenetbot.show_postprocessing_status(client, message)
+    
+    									    									    									
+@Client.on_message(filters.command(["status", "dstatus"]) & check_auth)
+async def postprocessing_status(client: Client, message: Message):
+    await usenetbot.show_downloading_status(client, message)
+    
+    
+@Client.on_message(filters.command(["resumeall", "pauseall", "cancelall"]) & sudo_cmd)
+async def sudo_cmds(client: Client, message: Message):
+    
+    command = message.command[0]    
+    if command == "resumeall":
+    	if usenetbot.resumeall_task():
+            return await message.reply_text("Resumed all Task successfully.", quote= True)
+          
+    elif command == "pauseall":
+    	if usenetbot.pauseall_task():
+            return await message.reply_text("Paused all Task successfully.", quote=True)
+            
+    elif command == "cancelall":
+    	if usenetbot.deleteall_task():
+            return await message.reply_text("Cancelled all Task successfully.", quote=True)
+            
+    return await message.reply_text("something went wrong.", quote=True)    
+         	                                                                               
+                                                                                    
+                                        
+@Client.on_message(filters.command("resume") & check_auth)
+async def resume_task(client: Client, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("Send Task ID with the command to resume the Task.")
+    
+    user_id, user_input = message.from_user.id, message.text.split(None, 1)[1]
+    if (user_id not in sabnzbd_userid_log) and (user_id not in SUDO_USERID):
+    	return await message.reply_text("No Task found under your User ID.", quote=True)
+    
+    if user_id not in SUDO_USERID:
+    	if user_input not in sabnzbd_userid_log[user_id]:
+    		return await message.reply_text("No Task found with that Task ID under your User ID.", quote=True)
+    	
+    result = usenetbot.resume_task(task_id=user_input)
+    if result:
+    	return await message.reply_text(f"Task {user_input} successfully resumed.", quote=True)
+    else: return await message.reply_text("No Task found with that Task ID .", quote=True)
+   
+   
+   
+@Client.on_message(filters.command("pause") & check_auth)
+async def pause_task(client: Client, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("Send Task ID with the command to  the Task.")
+    
+    user_id, user_input = message.from_user.id, message.text.split(None, 1)[1]  
+    if (user_id not in sabnzbd_userid_log) and (user_id not in SUDO_USERID):
+    	return await message.reply_text("No Task found under your User ID.", quote=True)
+    
+    if user_id not in SUDO_USERID:
+    	if user_input not in sabnzbd_userid_log[user_id]:
+    		return await message.reply_text("No Task found with that Task ID under your User ID.", quote=True)
+    	
+    result = usenetbot.pause_task(task_id=user_input)
+    if result:
+    	return await message.reply_text(f"Task {user_input} successfully paused.", quote=True)
+    else: return await message.reply_text("No Task found with that Task ID .", quote=True)
+   
  
+           	  	       	  		          	  	       	  		
+@Client.on_message(filters.command(["delete", "cancel"]) & check_auth)
+async def delete_task(client: Client, message: Message):
+    
+    if len(message.command) < 2:
+        return await message.reply_text("Send Task ID with the command to delete the Task.")
+    
+    user_id, user_input = message.from_user.id, message.text.split(None, 1)[1]
+    if (user_id not in sabnzbd_userid_log) and (user_id not in SUDO_USERID):
+    	return await message.reply_text("No Task found under your User ID.", quote=True)
+    
+    if user_id not in SUDO_USERID:
+    	if user_input not in sabnzbd_userid_log[user_id]:
+    		return await message.reply_text("No Task found with that Task ID under your User ID.", quote=True)
+    	
+    result = usenetbot.delete_task(task_id=user_input)
+    if result:
+    	return await message.reply_text(f"Task {user_input} successfully deleted.", quote=True)
+    	
+    else: return await message.reply_text("No Task found with that Task ID .", quote=True)
+   
 
 
-downloading_status_chatids = {} #saves chatid where downloading status page is active.
-postprocess_status_chatids = {} #saves chatid where postprocessing status page is active. 
-	
-	
-class UsenetBot:
-	
-    def __init__(self):
-    	#Number of blocks in progress bar,
-    	self.__number_of_blocks = 11
+@Client.on_message(filters.command("nzbmirror") & check_auth)
+async def nzbmirror(client: Client, message: Message):
+    
+    userid = message.from_user.id
+    replied_message = message.reply_to_message
+    
+    if not replied_message:
+    	return await message.reply_text("Reply to a proper NZB file to mirror.", quote=True)    	
+    
+    replymsg  = await message.reply_text("Adding your NZB file. Please Wait...",  quote=True)
+    if not replied_message.document:
+    	return await replymsg.edit("Reply to a proper NZB file to mirror.")
+    
+    if not all([bool(replied_message.document), ".nzb" in replied_message.document.file_name]):
+    	return await replymsg.edit("Reply to a proper NZB file to mirror.")
     	
-    	#progress bar ascii animation
-    	self.__remaining_block_ascii = "▱"
-    	self.__completed_block_ascii = "▰"
-    	    	
-    	self.SABNZBD_API = f"{SABNZBD_ENDPOINT}&output=json"
-    	self.client =  httpx.AsyncClient()
+    download_path = await message.reply_to_message.download()    
+    result = await usenetbot.add_nzbfile(path_name=download_path)    
+       
+    if result["status"]:
+    	sabnzbd_userid_log.setdefault(userid, []).append(result["nzo_ids"][0])        	
+    	asyncio.create_task(usenetbot.show_downloading_status(client, message))
+    	return await replymsg.edit("Your NZB file is successfuly Added in Queue.")        		        		
+    return replymsg.edit("Something went wrong while processing your NZB file.")
+    
 
-  	    	
-    def footer_message(self, speed=None):
-    	botuptime = get_readable_time(timefunc() - BotStartTime)
-    	total, used, free = disk_usage(".")
-    	free_space = get_readable_bytes(free)
-    	total_space = get_readable_bytes(total)
-    	cpu_usage= f"{cpu_percent()}%"
-    	ram_usage= f"{virtual_memory().percent}%"
-    	
-    	msg = "━━━━━━━━━━━━━━━━━━━━\n"
-    	msg += f"CPU: {cpu_usage} | RAM: {ram_usage}  \n"
-    	msg += f"Total: {total_space} | Free: {free_space}\n"
-    	msg += f"UPTIME: {botuptime} | DL-Speed: {speed}\n"
-    	return msg
-    	 
-    	    	    	    	    	   	    	    	    	
-    async def downloading_status_page(self):        
-	    """Generate status page for downloading progress message."""
-	    
-	    response = await self.client.get(self.SABNZBD_API, params={'mode':'queue'}).json()
-	    if not response["queue"]["slots"]: return None  
-	    
-	    queue_list = response["queue"]["slots"]
-	    number_of_task = len(queue_list)
-
-	    status_page = ""
-	    for index, queue in enumerate(queue_list):
-	    	filled_blocks = round(int(queue['percentage']) * self.__number_of_blocks / 100)
-	    	unfilled_blocks = self.__number_of_blocks - filled_blocks
-	    	
-	    	file_name = queue['filename']
-	    	if re.search(r"(http|https)", queue['filename']):
-	    		file_name = "Adding file from Link."
-	    	
-	    	msg =  f"**📂 FileName:** __{file_name}__\n"
-	    	msg += f"**{queue['percentage']}%**  `[{self.__completed_block_ascii * filled_blocks}{self.__remaining_block_ascii * unfilled_blocks}]`\n"
-	    	msg += f"**{queue['sizeleft']}** __remaining of__ **{queue['size']}**\n"
-	    	msg += f"**Status:** {queue['status']} | **ETA:** {queue['timeleft']}\n"
-	    	msg += f"**Task ID:** `{queue['nzo_id']}`\n\n"		
-	    	status_page += msg
-	    	
-	    	if index == 5:
-	    		status_page += f"+ {number_of_task-5} Ongoing Task...\n\n"
-	    		break
-	     			
-	    status_page += self.footer_message(response["queue"]["speed"])
-	    return status_page
-	    
-    async def postprocessing_status_page(self):
-    	"""Generate status page for postprocessing progress message."""
-    	
-    	response = await self.client.get(self.SABNZBD_API, params={'mode':'history'}).json()
-    	if not response["history"]["slots"]: return None
-    	
-    	history_list = response["history"]["slots"]
-    	
-    	status_page = ""
-    	for index, history in enumerate(history_list):
-    	    if history["status"] not in ["Completed", "Failed"]:
-    	    	msg = f"**📂 FileName :** __{history['name']}__\n"
-    	    	msg += f"**Status:** {queue['status']} | **ETA:** {queue['timeleft']}\n"
-    	    	action= history.get('action_line', None)
-    	    	if isinstance(action, list) and action_line:	
-    	    		msg += f"**Action:** {action[0]}\n"
-    	    		
-    	    	if action:
-    	    	    if "Running script:" in action:
-    	    	    	action = action.replace("Running script:", "")
-    	    	    msg += f"**Action :** __{action.strip()}__\n"
-    	    	        	    	        	    	
-    	    	msg+= "\n"
-    	    	status_page += msg    	    	
-	    
-    	if status_page: status_page += self.footer_message()    		
-    	return status_page
-	  	  	  	  
-	  	  	  	  
-    async def check_task(self, task_id):
-        response = await self.client.get(self.SABNZBD_API, params={'mode':'queue', 'nzo_ids': task_id}).json() 
-        return bool(response["queue"]["slots"])
-
-                
-    async def get_task(self, task_id):
-        response = await self.client.get(self.SABNZBD_API, params={'mode':'queue', 'nzo_ids': task_id}).json() 
-        return bool(response["queue"]["slots"])     
         
-                	    	   	    	            	    	   	    	    
-    async def resume_task(self, task_id):
-    	isValidTaskID =await self.check_task(task_id)
-    	if not isValidTaskID: return False
-    	
-    	return await self.client.get(self.SABNZBD_API, 
-    	       params={'mode':'queue', 'name':'resume', 'value': task_id}).json()
-
-    	           	       
-    async def resumeall_task(self):
-        response =await self.client.get(self.SABNZBD_API, params={'mode':'resume'}).json() 
-        return response["status"]
- 
-        	                            	                           	                            	                    
-    async def pause_task(self, task_id):
-    	isValidTaskID = self.check_task(task_id)
-    	if not isValidTaskID: return False
-    	    	
-    	return await self.client.get(self.SABNZBD_API, 
-    	       params={'mode':'queue', 'name':'pause', 'value': task_id}).json()
-
-    	           	       
-    async def pauseall_task(self):
-        response = self.client.get(self.SABNZBD_API, params={'mode':'pause'}).json()
-        return response["status"]      
-
-        	        	
-    async def delete_task(self, task_id):
-    	isValidTaskID = self.check_task(task_id)
-    	if not isValidTaskID: return False
-    	    	
-    	return await self.client.get(self.SABNZBD_API, 
-    	       params={'mode':'queue', 'name':'delete', 'value': task_id}).json()
+@Client.on_message(filters.command(["nzbgrab", "nzbadd"]) & check_auth)
+async def nzbgrab(client: Client, message: Message):
     
-    	       	       
-    async def deleteall_task(self):
-        response = await self.client.get(self.SABNZBD_API, params={'mode':'queue', 'name':'delete', 'value':'all'}).json()
-        return response["status"]
-
-
-    async def add_nzbfile(self, path_name):
-        try:
-        	async with aiofiles.open(path_name, "rb") as file:
-        		nzb_content = await file.read()
-        except: return False
+    userid = message.from_user.id 
+    if len(message.command) < 2:
+    	return await message.reply_text("Please provide a proper ID", quote=True)
+    	
+    user_input = message.text.split(maxsplit=1)[1]    
+    nzbhydra_idlist = re.findall(r"-?\b[0-9]+\b", user_input)    
+    
+    if not nzbhydra_idlist:
+    	return await message.reply_text("Please provide a proper ID.", quote=True)
+    	
+    replymsg  = await message.reply_text("Adding your requested ID(s). Please Wait...",  quote=True)
+    
+    success_taskids = []
+    for id in nzbhydra_idlist:
+        nzburl = NZBHYDRA_URL_ENDPOINT.replace("replace_id", id)
+        async with httpx.AsyncClient() as client:
+        	response = await client.head(nzburl)
         
-        payload = {'nzbfile': (path_name.split("/")[-1], nzb_content) }  
-        params = {'mode':'addfile'}
-        response = await self.client.post(self.SABNZBD_API, params=params ,files=payload)
-        return response.json()
+        if "Content-Disposition" in response.headers:
+        	result = usenetbot.add_nzburl(nzburl)
+        
+        if result["status"]:
+        	success_taskids.append(result['nzo_ids'][0])
 
-
-    async def add_nzburl(self, nzburl):
-        params = {'mode':'addurl', "name":nzburl}
-        response = await self.client.post(self.SABNZBD_API, params=params)
-        return response.json()      	        	      	        	
-
-      	     	    	    	     	    	    	                	     	    	    	     	    	    	          
-    async def clear_progresstask(self, status_message,  chat_id,  progress):
-    	"""remove job, delete message and clear dictionary of progress bar."""
-    	
-    	scheduler.remove_job(f"{progress}_{str(chat_id)}")
-    	try: await status_message.delete()
-    	except: pass #passing errors like status message deleted.
-    	
-    	if progress == "downloading":
-    		downloading_status_chatids.pop(chat_id)
-    	elif progress == "postprocessing":
-    		postprocess_status_chatids.pop(chat_id)
-
-    		       		   
-    async def show_downloading_status(self, client, message):
-    	chat_id = message.chat.id
-    	
-    	# Remove previous status message and scheduled job for that chat_id    	
-    	if chat_id in downloading_status_chatids:
-    		message_id = downloading_status_chatids[chat_id]
-    		status_message= await client.get_messages(chat_id, message_id)
-    		await self.clear_progresstask(status_message, chat_id,  progress="downloading")
-    		    			   			    			    			    		
-    	# Get the status page
-    	status_page = await self.downloading_status_page()    	    	    	
-    	if not status_page:
-            return await client.send_message(chat_id, "No ongoing task currently." , reply_to_message_id=message.id)
-    		     	
-    	# Send the status message and start the job to update the downloading status message after x interval.
-    	status_message= await client.send_message(chat_id, status_page, reply_to_message_id=message.id)
-    	downloading_status_chatids[chat_id] = status_message.id
-    
-    	async def edit_status_message():
-    		"""Edit the status message  after x seconds."""
-    				
-    		status_page = await self.downloading_status_page()
-    		if not status_page:
-    			return await self.clear_progresstask(status_message, chat_id,  progress="downloading")
-    			
-    		try: await status_message.edit(status_page)
-    		except: await self.clear_progresstask(status_message, chat_id,  progress="downloading")
-    		
-    	scheduler.add_job(edit_status_message, "interval", seconds=10, misfire_grace_time=15,max_instances=2, id=f"downloading_{str(chat_id)}")
-  
-   	  	
-    async def show_postprocessing_status(self, client, message):
-    	chat_id = message.chat.id 
-    	
-    	# Remove previous status message and scheduled job for that chat_id    	
-    	if chat_id in postprocess_status_chatids:
-    		message_id = postprocess_status_chatids[chat_id]
-    		status_message= await client.get_messages(chat_id, message_id)
-    		await self.clear_progresstask(status_message, chat_id,  progress="postprocessing")
-    		    			   			    			    			    		
-    	# Get the status page
-    	status_page = await self.postprocessing_status_page()    	    	    	
-    	if not status_page:
-    		return await client.send_message(chat_id, "No ongoing post processing task found." , reply_to_message_id=message.id)
-    		     	
-    	# Send the status message and start the job to update the postprocessing status message after x interval.
-    	status_message= await client.send_message(chat_id, status_page, reply_to_message_id=message.id)
-    	postprocess_status_chatids[chat_id] = status_message.id
-    
-    	async def edit_status_message():
-    		"""Edit the status message  after x seconds."""
-    				
-    		status_page = await self.postprocessing_status_page()	
-    		if not status_page:
-    		    return await self.clear_progresstask(status_message, chat_id,  progress="postprocessing")
-    				    			    						
-    		try: await status_message.edit(status_page)
-    		except Exception as error:
-    			return await self.clear_progresstask(status_message, chat_id,  progress="postprocessing")
-    			
-    	scheduler.add_job(edit_status_message, "interval", seconds=10, misfire_grace_time=15,max_instances=2, id=f"postprocessing_{str(chat_id)}")
+       		
+    if success_taskids:
+        sabnzbd_userid_log.setdefault(userid, []).extend(success_taskids)
+        asyncio.create_task(usenetbot.show_downloading_status(client, message))
+        
+        await replymsg.delete()
+        return await message.reply_text(f"{len(success_taskids)} Tasks have been successfully added.", quote=True)
+       
+    return await replymsg.edit("No task has been added.")
+       	
