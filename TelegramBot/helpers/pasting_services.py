93216c2fb6106b2b8e0195bd944aa0f548b09064
borxR <94912459+borxR@users.noreply.github.com>
2023-04-02 12:43:30 +0530

    use katb.in api (#9)

--- TelegramBot/helpers/pasting_services.py
+++ TelegramBot/helpers/pasting_services.py
@@ -8,17 +8,15 @@ async def katbin_paste(text: str) -> str:
     paste the text in katb.in website.
     """
 
-    katbin_url = "https://katb.in"
+    katbin_url = "https://katb.in/api/paste"
     client = AsyncClient()
-    response = await client.get(katbin_url)
-    soup = BeautifulSoup(response.content, "html.parser")
-    csrf_token = soup.find("input", {"name": "_csrf_token"}).get("value")
     try:
         paste_post = await client.post(
             katbin_url,
-            data={"_csrf_token": csrf_token, "paste[content]": text},
-            follow_redirects=False)
-        output_url = f"{katbin_url}{paste_post.headers['location']}"
+            json={"paste": {"content": f"{text}"}}).json()
+        
+        output_url = "https://katb.in/{}".format(paste_post["id"])
+        
         await client.aclose()
         return output_url
     except:
